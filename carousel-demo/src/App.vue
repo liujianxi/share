2d0b9167dd06dde682d4614950c1c9dce32ef3bc
liujianxi <13380360847@163.com>
2018-04-29 22:50:08 +0800

    跑马灯

---
+++ carousel-demo/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+	<div id="app">
+		<keep-alive>
+			<router-view/>
+		</keep-alive>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'App'
+	}
+</script>
+
+<style>
+	body,
+	html {
+		margin: 0;
+		padding: 0;
+	}
+	
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+	
+	ul,
+	li {
+		list-style: none;
+		padding: 0;
+		margin: 0;
+	}
+</style>
---
+++ carousel-demo/src/components/home.vue
@@ -0,0 +1,85 @@
+<template>
+	<div class="home">
+		<div class="content">
+			<ul :style="{'left': -500*activeNum+'px',}">
+				<li v-for="(item,index) in imgData"><img :src="item.src" /></li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+	import a from '../assets/1.jpg'
+	import b from '../assets/2.jpg'
+	import c from '../assets/3.jpg'
+	import d from '../assets/4.jpg'
+	export default {
+		name: 'HelloWorld',
+		data() {
+			return {
+				activeNum: 0,
+				timer: '',
+				a:a,
+				imgData: [{
+					src: a,
+				}, {
+					src: b,
+				},{
+					src: c,
+				},{
+					src: d,
+				}]
+			}
+		},
+		created() {
+			this.timeStart();
+		},
+		activated() {
+
+		},
+		methods: {
+			timeStart() {
+				let self = this;
+				self.timer = setInterval(res => {
+					self.activeNum += 1;
+					console.log(self.activeNum);
+					if(self.activeNum==3){
+						self.imgData.shift()
+					}
+				}, 3000)
+			},
+		}
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+	
+	h1,
+	h2 {
+		font-weight: normal;
+	}
+	.content{
+		width: 500px;
+		height: 379px;
+		margin: 0 auto;
+		position: relative;
+		overflow: hidden;
+	}
+	ul {
+		position: absolute;
+		left: 0;
+		top: 0;
+		display: flex;
+		display: -webkit-flex;
+		/*animation: move 1s linear;*/
+		transition: all 1s linear;
+		li {
+			flex: 1;
+			-wekbit-flex: 1;
+		}
+	}
+	a {
+		color: #42b983;
+	}
+</style>
